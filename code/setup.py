4ff99f2144bf280c8b7b782f16f1c189bae5d4a6
Maral-Pn <pourdayan.ma.trd@gmail.com>
2024-08-29 23:15:54 +1000

    setup file added.

---
+++ code/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='pipeline-people',
+      version='0.0.1',
+      description='Sample pyspark project',
+      packages=find_packages())
+
